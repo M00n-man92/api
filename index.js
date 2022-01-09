fd58e6f947435f53ece1ac35a589686ce68414e9
M00n-man92 <azraelnumb00@gmail.com>
2022-01-09 17:23:54 +0300

    three try

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('./mongoose');
 const express = require('express');
 const mongoose = require('./mongoose');
 const app = express();
-
+const path=require('path')
 const dotenv = require('dotenv')
 
 dotenv.config()
