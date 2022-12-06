6cfa2a7f290e13ad6dc579466e6012fadca872e1
M00n-man92 <azraelnumb00@gmail.com>
2022-12-06 12:13:01 +0300

    yeah

--- routes/user.js
+++ routes/user.js
@@ -158,7 +158,6 @@ route.post('/anothercoffee', async (req, res) => {
 			}
 		});
 		const options = {
-			
 			from: 'SheramiDev@outlook.com', // sender address
 			to: "contact@diligentts.com", // list of receivers
 			subject: ` NAME *** ${name} ***
